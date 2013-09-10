8136c5e003d5767dfe1691dc8e4712e69db42861
Alexander Skrinnik <askrinnik@metratech.com>
2013-09-10 08:19:04 -0400

    Add GitRevert Changes in PullInAllFolder.bat

--- PullInAllFolder.bat
+++ PullInAllFolder.bat
@@ -1,11 +1,12 @@
 @pushd c:\Dev\MetraNet
 
 @echo %time%
+@set BeginTime=%time%
 
 git pull -v --progress  "origin"
 git submodule foreach git pull -v --progress  "origin"
 
-@echo %time%
+@echo From %BeginTime% to %time%
 
 @popd
 @pause
