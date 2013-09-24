eea4e1c209d7b74eb62fd73bd954e929bf5e1c29
Alexander Skrinnik <askrinnik@metratech.com>
2013-09-24 02:29:07 -0400

    Commandline parameter was added to BuildByTarget.bat

--- BuildByTarget.bat
+++ BuildByTarget.bat
@@ -1,5 +1,5 @@
 @echo %time%
 @set StartTime=%time%
-msbuild %ROOTDIR%\Build\MSBuild\MetraNetBuild.proj /m /ds /t:IncrementalCs /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild.log /clp:Verbosity=M;Summary
+msbuild %ROOTDIR%\Build\MSBuild\MetraNetBuild.proj /m /ds /t:%1 /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild.log /clp:Verbosity=M;Summary
 @echo From %StartTime% to %time%
 pause
