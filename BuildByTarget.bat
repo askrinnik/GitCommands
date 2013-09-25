0518830483ac2d3fec3e980548ce5204bb52d066
Alexander Skrinnik <askrinnik@metratech.com>
2013-09-25 01:36:55 -0400

    MetraNet command files were moved to MTCommands repository

--- BuildByTarget.bat
+++
@@ -1,5 +0,0 @@
-@echo %time%
-@set StartTime=%time%
-msbuild %ROOTDIR%\Build\MSBuild\MetraNetBuild.proj /m /ds /t:%1 /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild.log /clp:Verbosity=M;Summary
-@echo From %StartTime% to %time%
-pause
