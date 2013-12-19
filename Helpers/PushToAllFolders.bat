9ca36ed9cbf1a36f7fdc527fd5b09823ea7362fe
Alexander Skrinnik <askrinnik@metratech.com>
2013-12-19 08:08:30 -0500

    Fix PushToAllFolders.bat for MetraNetDev suffix

--- Helpers/PushToAllFolders.bat
+++ Helpers/PushToAllFolders.bat
@@ -6,7 +6,7 @@
 @pushd %DEVDIR%
 
 @set branchName=%1
-@if NOT "%branchName%" == "develop" set branchName=%branchName%@MetraNet
+@if NOT "%branchName%" == "develop" set branchName=%branchName%@MetraNetDev
 
 git submodule foreach git push -v --progress  "origin" %branchName%:%branchName%
 git push -v --progress  "origin" %1:%1
