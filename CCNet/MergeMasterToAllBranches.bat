756642e95c05240416c5172764cd96cf1aecbca4
Alexander Skrinnik <askrinnik@metratech.com>
2014-04-23 02:43:53 -0400

    Add new branches to CCNet

--- CCNet/MergeMasterToAllBranches.bat
+++ CCNet/MergeMasterToAllBranches.bat
@@ -1,2 +1,11 @@
 call MergeMasterToBranch.bat feature/DevX
 call MergeMasterToBranch.bat feature/CMI
+call MergeMasterToBranch.bat feature/UiUx
+call MergeMasterToBranch.bat feature/Team3-710
+call MergeMasterToBranch.bat feature/TeamTA
+
+@pushd %DEVDIR%\..\CCNet
+git.exe checkout feature/DevX
+@popd
+
+
