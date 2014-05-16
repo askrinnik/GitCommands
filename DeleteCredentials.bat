937cfa81ff0985acafd66c08418f79434501bccb
Alexander Skrinnk <askrinnik@metratech.com>
2014-05-16 09:41:17 -0400

    Add a script for credentials clearing

---
+++ DeleteCredentials.bat
@@ -0,0 +1,5 @@
+git config --global --unset-all metragit.user
+git config --global --unset-all metragit.token
+git config --global --unset-all user.name
+git config --global --unset-all user.email
+pause
