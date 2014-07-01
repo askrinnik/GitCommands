2cfbfccdc743a80769636fe5e9511d2b4ce9f55c
Alexander Skrinnik <askrinnik@metratech.com>
2014-07-01 03:04:13 -0400

    Set branch for pull

--- CCNet/MergeMasterToBranch.bat
+++ CCNet/MergeMasterToBranch.bat
@@ -1,7 +1,7 @@
 @pushd %DEVDIR%\..\CCNet
 git.exe checkout %1
-git.exe pull -v --progress  "origin"
+git.exe pull -v --progress "origin" %1
 git.exe merge master
-git.exe push -v --progress  "origin" %1
+git.exe push -v --progress "origin" %1
 @popd
 @pause
