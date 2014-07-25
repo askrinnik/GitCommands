9a0bd39a713a0680c53f39c46988f9c3d93755bd
Alexander Skrinnik <askrinnik@metratech.com>
2014-07-25 06:16:37 -0400

    Add branch functionality added

---
+++ Helpers/CreateBranchesWithSubmodules.bat
@@ -0,0 +1,10 @@
+@pushd %DEVDIR%
+
+git.exe checkout -b %1
+git.exe submodule foreach git checkout -b %1@MetraNetDev
+
+git.exe push origin %1
+git.exe submodule foreach git push origin %1@MetraNetDev
+
+pause
+
