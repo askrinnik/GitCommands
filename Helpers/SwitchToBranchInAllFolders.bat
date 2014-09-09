24294f5e4217718b896f4afa3d0df68315cff202
Alexander Skrinnik <askrinnik@metratech.com>
2014-09-09 04:24:53 -0400

    Add skip pause support

--- Helpers/SwitchToBranchInAllFolders.bat
+++ Helpers/SwitchToBranchInAllFolders.bat
@@ -1,11 +1,19 @@
 @pushd %DEVDIR%
 
 git checkout %1
+@if not %errorlevel%==0 (
+  exit /b %errorlevel%
+)
 
 @set branchName=%1
 @if NOT "%branchName%" == "develop" set branchName=%branchName%@MetraNetDev
 
 git submodule foreach git checkout "%branchName%"
 
+@if not %errorlevel%==0 (
+  exit /b %errorlevel%
+)
+
 @popd
+@IF NOT "%2%"=="skip_pause" (
 @pause
