c81f4f116122dbc8121175a6b7ddd47e6f937ffe
Alexander Skrinnik <askrinnik@metratech.com>
2014-12-12 06:50:39 -0500

    DEVX-380 Add CC.NET project to merge team branch to develop. Redirect output streams

--- Helpers/PushWithSubmodules.bat
+++ Helpers/PushWithSubmodules.bat
@@ -2,7 +2,7 @@
 @set isSkipPause=%2
 
 @echo .
-@echo push to %1 
+@echo push to %branchName% 
 @echo .
 @if not "%isSkipPause%"=="skip_pause" pause
 
@@ -11,16 +11,16 @@
 @set branchNameSubmodules=%branchName%
 @if NOT "%branchNameSubmodules%" == "develop" set branchNameSubmodules=%branchNameSubmodules%@MetraNetDev
 
-git submodule foreach git push -v --progress  "origin" %branchNameSubmodules%:%branchNameSubmodules%
+git submodule foreach git push -v --progress  "origin" %branchNameSubmodules%:%branchNameSubmodules% 2>&1
 @if not %errorlevel%==0 (
-  echo git submodule foreach git push error 1>&2
+  echo Error in git submodule foreach git push -v --progress  "origin" 1>&2
   @if not "%isSkipPause%"=="skip_pause" pause
   exit /b %errorlevel%
 )
 
-git push -v --progress  "origin" %branchName%:%branchName%
+git push -v --progress  "origin" %branchName%:%branchName% 2>&1
 @if not %errorlevel%==0 (
-  echo git push error 1>&2
+  echo Error in git push -v --progress  "origin" %branchName%:%branchName% 1>&2
   @if not "%isSkipPause%"=="skip_pause" pause
   exit /b %errorlevel%
 )
