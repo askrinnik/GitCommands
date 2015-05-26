20a9f6a78638ceb2739dad73d609c8ae90f171c0
Alexander Skrinnik <askrinnik@metratech.com>
2015-05-26 15:27:08 -0400

    Fix path

--- GitRevert.bat
+++ GitRevert.bat
@@ -1,9 +1,11 @@
 rem if you need skip pause after executions call "SetUnchangeConfigFiles.bat skip_pause"
+@set currentDir=%~dp0
+
 @pushd %DEVDIR%
 git reset --hard
 git submodule foreach git reset --hard
 @popd
 
 if NOT "%1%"=="skip_set_unchange_config" (
-call SetUnchangeConfigFiles.bat %1%
+call %currentDir%SetUnchangeConfigFiles.bat %1%
 )
