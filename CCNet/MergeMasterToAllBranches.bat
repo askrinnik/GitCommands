db006d23147998540210a6499083d2a96856f908
Alexander Skrinnik <askrinnik@metratech.com>
2015-09-02 01:32:58 -0400

    Update branches list

--- CCNet/MergeMasterToAllBranches.bat
+++ CCNet/MergeMasterToAllBranches.bat
@@ -7,12 +7,13 @@ call MergeMasterToBranch.bat feature/DevX
 call MergeMasterToBranch.bat feature/CMI
 call MergeMasterToBranch.bat feature/Billing
 call MergeMasterToBranch.bat feature/TeamTA
-call MergeMasterToBranch.bat feature/UiUx
 call MergeMasterToBranch.bat feature/Sust2
 call MergeMasterToBranch.bat feature/Engines
 call MergeMasterToBranch.bat feature/Reporting
 call MergeMasterToBranch.bat feature/devops
 call MergeMasterToBranch.bat develop
+call MergeMasterToBranch.bat package/8.2
+call MergeMasterToBranch.bat release/8.2
 
 @pushd %DEVDIR%\..\CCNet
 git.exe checkout feature/DevX
