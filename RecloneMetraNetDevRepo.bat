7c30cc020e11b995a7eb9041685631edb5c25ebd
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2016-04-13 09:01:48 -0400

    Improve Reclonning process

--- RecloneMetraNetDevRepo.bat
+++ RecloneMetraNetDevRepo.bat
@@ -1,31 +1,35 @@
-set BRANCH=%1
+set BranchName=%1
+set CurrentFolder=%~dp0
 
-if '%BRANCH%' == '' set BRANCH=develop
-call %ROOTDIR%\Build\Tools\StopAllServices.bat
+if '%BranchName%' == '' set BranchName=develop
+if exist "%ROOTDIR%\Build\Tools\StopAllServices.bat" call %ROOTDIR%\Build\Tools\StopAllServices.bat
 if exist "%DEVDIR%" rd /s /q "%DEVDIR%"
 
-git.exe clone -q --recursive "https://github.com/MetraTech/MetraNetDev.git" "%DEVDIR%"
-@if %errorlevel% gtr 0 (goto error)
+git.exe clone -q --recursive "https://github.com/MetraTech/MetraNetDev.git" "%DEVDIR%" -b %BranchName% 2>&1
+@if not %errorlevel%==0 (
+  echo Error in git clone 1>&2
+  exit /b %errorlevel%
+)
 
 @pushd "%DEVDIR%"
 
 git.exe submodule update --init 
-@if %errorlevel% gtr 0 (goto error)
+@if not %errorlevel%==0 (goto error)
 
-call %ROOTDIR%\Build\Tools\Git\SwitchToBranchInAllFolders.bat %BRANCH% skip_pause
-@if %errorlevel% gtr 0 (goto error)
+call %CurrentFolder%SwitchToBranchInAllFolders.bat %BranchName% skip_pause
+@if not %errorlevel%==0 (goto error)
 
-call %ROOTDIR%\Build\Tools\Git\PullInAllFolders.bat skip_pause 
-@if %errorlevel% gtr 0 (goto error)
+call %CurrentFolder%PullInAllFolders.bat skip_pause hide_progress
+@if not %errorlevel%==0 (goto error)
 
 @echo Reclonning succeed
 @goto end
 
 :error
-@echo Reclonning failed
+@echo Reclonning failed 1>&2
+@exit /b 1
 
 :end
-@pause
 
 
 
