8abab48d2afb690203a1e5a2f53be5d5ecef5a18
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2016-10-13 10:39:28 +0300

    Add variables

--- SetupRepoVariables.bat
+++ SetupRepoVariables.bat
@@ -1,5 +1,8 @@
-set DEVDIR=C:\dev\MetraNetDev
+set DEVDIR=d:\dev\MetraNetDev
 setx DEVDIR "%DEVDIR%" /M
+
+set ROOTDIR=%DEVDIR%\Source
+setx ROOTDIR %ROOTDIR% /M
 
 set GitCmdMNDev=C:\dev\GitCommands
 setx GitCmdMNDev "%GitCmdMNDev%" /M
